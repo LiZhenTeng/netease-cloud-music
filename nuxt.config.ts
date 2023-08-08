afada5d0d5443a9c74a9f0a34645054fca532c0c
星星魔法术 <xingxingmofashu@outlook.com>
2023-08-08 15:29:10 +0800

    fix:APlayer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,6 +30,13 @@ export default defineNuxtConfig({
   pinia: {
     autoImports: ['defineStore', 'acceptHMRUpdate'],
   },
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ['APlayer']
+      }
+    }
+  },
   appConfig: {
     limit: 24
   },
